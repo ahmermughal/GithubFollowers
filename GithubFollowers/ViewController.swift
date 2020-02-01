dfa616b5d26da81a062d55f0fa97fad2502a33eb
Ahmer Mughal <a2mughal143@gmail.com>
2020-02-01 13:51:24 +0500

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Ahmer Mughal on 01/02/2020.
+//  Copyright © 2020 iDevelopStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
