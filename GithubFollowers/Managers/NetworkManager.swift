df940e16efd5c0bd125b09897553fb0a342189bb
Ahmer Mughal <a2mughal143@gmail.com>
2020-02-27 22:18:02 +0500

    changed network manager closure to Result Type

--- GithubFollowers/Managers/NetworkManager.swift
+++ GithubFollowers/Managers/NetworkManager.swift
@@ -15,31 +15,31 @@ class NetworkManager{
     
     private init(){}
     
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void){
+    func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void){
         let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else{
             // if we have a bad url then we return the error
-            completed(nil, ErrorMessage.invalidUsername)
+            completed(.failure(.invalidUsername))
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             // if error is not null
             if let _ = error{
-                completed(nil, .unableToComplete)
+                completed(.failure(.unableToComplete))
                 return
             }
             
             // if this response is not nill and the status code is 200
             // else response is nill
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
-                completed(nil, .invalidResponse)
+                completed(.failure(.unableToComplete))
                 return
             }
             
             guard let data = data else {
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
                 return
             }
             
@@ -47,9 +47,9 @@ class NetworkManager{
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
-                completed(followers, nil)
+                completed(.success(followers))
             }catch{
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
             }
         }
         
